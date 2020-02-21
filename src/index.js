b1cd19eed4c998862366115cb4b4f0b1e663201d
Brent Groves <brent.groves@gmail.com>
2020-02-21 08:47:10 -0500

    Added/Update source code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@
 const logger = require('./logger');
 const app = require('./app');
 
-const mqtt = require('mqtt');
-//const config = require('../../Config13319/config.json');
-//const hostname = app.get('host');
-//const port = app.get('port');
-//const port = config.BPGServicesPort;
 const port = process.env.FEATHERS_PORT;
-//const server = app.listen(port, 'localhost');
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-//const server = app.listen(port, hostname);
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
@@ -20,31 +15,21 @@ server.on('listening', () =>
   logger.info('Feathers application started on port %d', port)
 );
 
-// For good measure let's create a message
-// So our API doesn't look so empty
-/*
-app.service('test').create({
-  text: 'Hello world from the server',
-});
-app.service('mstest').create({
-  text: 'Hello world from the server',
-});
-*/
 // TEST ONLY SECTION
-
-  app.service('users')
-    .create({
-      "email": "user@buschegroup.com",
-      "password": "password",
-      "userName": "bgroves",
-      "firstName": "Brent",
-      "lastName": "Groves",
-      "isAdmin": true,
-      "roles": [ "Admin", "Manager", "Quality"]
-
-
-    }).then(async (res) => {
-        console.log('created user!')
-    }).catch(e => {
-      console.error('Authentication error', e);
-    });
+app
+  .service('users')
+  .create({
+    email: 'user@buschegroup.com',
+    password: 'password',
+    userName: 'bgroves',
+    firstName: 'Brent',
+    lastName: 'Groves',
+    isAdmin: true,
+    roles: ['Admin', 'Manager', 'Quality']
+  })
+  .then(async res => {
+    console.log('created user!');
+  })
+  .catch(e => {
+    console.error('Authentication error', e);
+  });
