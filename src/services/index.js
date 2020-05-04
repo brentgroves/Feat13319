216dc4569f8ee6b5b4248deba2658867c0082566
Brent Groves <brent.groves@gmail.com>
2020-05-04 16:52:15 -0400

    Added/Updated code

--- src/services/index.js
+++ src/services/index.js
@@ -2,12 +2,14 @@ const users = require('./users/users.service.js');
 const sproc200206 = require('./sproc200206/sproc200206.service.js');
 const sproc200221 = require('./sproc200221/sproc200221.service.js');
 const sproc200311 = require('./sproc200311/sproc200311.service.js');
+const mysql200206 = require('./mysql200206/mysql200206.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(sproc200206);
   app.configure(sproc200221);
   app.configure(sproc200311);
+  app.configure(mysql200206);
 };
 /*
 module.exports = function (app) {
