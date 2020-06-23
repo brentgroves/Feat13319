e82494e8080a5539f0d15712361b5efd5169fd74
Brent Groves <brent.groves@gmail.com>
2020-06-23 13:02:04 -0400

    Added/Update source code

--- start-dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-( cd /home/node/App13319/Feat13319 && npm run all)
--- start-prod.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-( cd /home/node/App13319/Alarms13319 && ./start.sh) & (cd /home/node/App13319/Feat13319 && npm start)
